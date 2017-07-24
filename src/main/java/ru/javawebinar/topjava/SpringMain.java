ecec522d73df082eebbe19bd009409f5f2a25452
oleginnuendo@gmail.com <oleginnuendo@gmail.com>
2017-07-24 12:45:37 +0300

    start changes

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -8,13 +8,17 @@ import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
 
+
 public class SpringMain {
     public static void main(String[] args) {
-        // java 7 Automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+
+        try (ConfigurableApplicationContext appCtx =
+                     new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
+            adminUserController.create(new User(null, "userName", "email",
+                    "password", Role.ROLE_ADMIN));
         }
     }
 }
